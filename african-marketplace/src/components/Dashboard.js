1df87f20e2b97bdf6b3b2c567ac93b01af1879b5
Heorhii Siburov <g.siburov@gmail.com>
2019-09-22 17:37:13 -0700

    Added styling components, semantic ui, fonts

---
+++ african-marketplace/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Dashboard = () => {
+  
+  
+  
+    return (
+      <div className="Dashboard-block">
+          <h2>App ready to work</h2>
+          <p>Something important</p>
+      </div>
+    );
+  };
+  
+  export default Dashboard;
