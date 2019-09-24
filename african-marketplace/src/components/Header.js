351c104516913e9d9956c5d2206a9c816247410a
Heorhii Siburov <g.siburov@gmail.com>
2019-09-24 14:43:23 -0700

    Added SELL component

--- african-marketplace/src/components/Header.js
+++ african-marketplace/src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
       <div className="bottom-nav">
           <ul>
               <li><Link to="/dashboard">PRODUCTS</Link></li>
-              <li>SELL</li>
+              <li><Link to="/sell">SELL</Link></li>
               <li>MY ADS</li>
               <li onClick={logout}><Link to="/">LOG OUT</Link></li>
           </ul>
---
+++ african-marketplace/src/components/SellList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { axiosWithAuth } from "../utilites/axiosWithAuth";
+
+const SellList = () => {
+  
+  
+  
+    return (
+      <>
+        <p>Selltest</p>
+      </>
+    );
+  };
+  
+  export default SellList;
