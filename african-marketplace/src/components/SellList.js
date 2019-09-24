522eabe32fd047274f804407138d7e237a556680
Heorhii Siburov <g.siburov@gmail.com>
2019-09-24 14:53:05 -0700

    products list was reached from API

--- african-marketplace/src/components/SellList.js
+++ african-marketplace/src/components/SellList.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { axiosWithAuth } from "../utilites/axiosWithAuth";
 
 const SellList = () => {
   
-  
+    const [productsList, setProductsList] = useState([]);
+
+    useEffect(() => {
+        getData();
+      }, []);
+    
+    const getData = () => {
+    axiosWithAuth()
+        .get('/products')
+        .then(res => {
+            console.log("List of products received: ", res.data);
+            setProductsList(res.data);
+        })
+        .catch(err => console.log(err));
+};
+
   
     return (
       <>
