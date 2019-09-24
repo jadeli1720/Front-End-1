d01255f8c76f2fb40bf2d01c7529b730053c3fc0
Heorhii Siburov <g.siburov@gmail.com>
2019-09-24 13:40:37 -0700

    small fixes, added logout feature

--- african-marketplace/src/components/Header.js
+++ african-marketplace/src/components/Header.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
+
+  const logout = () => {
+    window.localStorage.removeItem('token')
+  }
+
   return (
     <nav className="header-container">
       <div className="bottom-nav">
           <ul>
-              <li>PRODUCTS</li>
+              <li><Link to="/dashboard">PRODUCTS</Link></li>
               <li>SELL</li>
               <li>MY ADS</li>
-              <li>LOG OUT</li>
+              <li onClick={logout}><Link to="/">LOG OUT</Link></li>
           </ul>
       </div>
     </nav>
