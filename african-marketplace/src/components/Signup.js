f17134e82320e1e6bb7a879775f939d954fbafd0
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-09-25 19:00:16 -0400

    started integrating validation logic into semantic ui form; having issues getting touched.values to be recognized (curently getting an error saying undefined)

--- african-marketplace/src/components/Signup.js
+++ african-marketplace/src/components/Signup.js
@@ -1,99 +1,151 @@
-import React from "react";
+import React, { setState } from "react";
 import axiosWithAuth from "../utilites/axiosWithAuth";
-
-import { Container, Header, Button, Form  } from 'semantic-ui-react';
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { Container, Header, Button, } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
 
-class Signup extends React.Component {
-  
-  state = {
-    credentials: {
-      username: '',
-      password: '',
-      first_name: '',
-      last_name: '',
-      email: ''
-    }
-  };
+const SignupForm = ({ values, errors, touched, isSubmitting }) => {
 
-  handleChange = e => {
-    this.setState({
-      credentials: {
-        ...this.state.credentials,
-        [e.target.name]: e.target.value
-      }
-    });
-  };
-  
-  login = e => {
-    e.preventDefault();
-    axiosWithAuth()
-      .post('/signup', this.state.credentials)
-      .then(res => {
-        console.log("Successful signup", res);
-        this.props.history.push('/');
-      })
-      .catch(err => console.log('Oh-oh, something wrong', err));
-  };
 
-  
-  render() {
   return (
     <Container text>
       <Header as='h2'>Welcome to the African Marketplace App!</Header>
       <Header as='h3'>Fill this from for new account</Header>
-      <Form onSubmit={this.login}>
-        <Form.Field>
-        <label>Login</label>
+
+
+
+
+      <Form>
+        <Field>
+          {touched.values.username && errors.values.username && <p className="error">{errors.values.username}</p>}
+          <label>Username</label>
           <input
             type="text"
             name="username"
-            value={this.state.credentials.username}
-            onChange={this.handleChange}
+
+
           />
-        </Form.Field>
-        <Form.Field>
-        <label>Password</label>
+
+        </Field>
+
+        <Field>
+          {touched.password && errors.password && (
+            <p className="error">{errors.password}</p>
+          )}
+          <label>Password</label>
           <input
             type="password"
             name="password"
-            value={this.state.credentials.password}
-            onChange={this.handleChange}
+
+
           />
-        </Form.Field>
-        <Form.Field>
-        <label>First Name</label>
+        </Field>
+
+        <Field>
+          {touched.first_name && errors.first_name && <p className="error">{errors.first_name}</p>}
+          <label>First Name</label>
           <input
             type="text"
             name="first_name"
-            value={this.state.credentials.first_name}
-            onChange={this.handleChange}
+
+
           />
-        </Form.Field>
-        <Form.Field>
-        <label>Last Name</label>
+        </Field>
+
+        <Field>
+          {touched.last_name && errors.last_name && <p className="error">{errors.last_name}</p>}
+          <label>Last Name</label>
           <input
             type="text"
             name="last_name"
-            value={this.state.credentials.last_name}
-            onChange={this.handleChange}
+
+
           />
-        </Form.Field>
-        <Form.Field>
-        <label>Email</label>
+        </Field>
+
+        <Field>
+          {touched.email && errors.email && (
+            <p className="error">{errors.email}</p>
+          )}
+          <label>Email</label>
           <input
             type="email"
             name="email"
-            value={this.state.credentials.email}
-            onChange={this.handleChange}
-          />
-        </Form.Field>
-          <Button primary type='submit'>Sign Up</Button>
-          <Button secondary><Link to="/">Cancel</Link></Button>
-        </Form>
-    </Container>
-  );
-  }
-};
 
+
+          />
+
+        </Field>
+
+        <Button primary type='submit' disabled={isSubmitting}>Sign Up</Button>
+        <Button secondary><Link to="/">Cancel</Link></Button>
+      </Form>
+
+
+
+
+
+    </Container >
+  );
+}
+
+
+
+const Signup = withFormik({
+  mapPropsToValues({ username, password, email, first_name, last_name }) {
+    return {
+      username: username || "",
+      password: password || "",
+      email: email || "",
+      first_name: first_name || "",
+      last_name: last_name || ""
+    }
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string().required("Username is required"),
+    password: Yup.string().min(6, "Password must be at least six characters").required("Password is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    first_name: Yup.string().required("Full name is required"),
+    last_name: Yup.string().required("Full name is required")
+  }),
+
+
+  /*handleChange(values, {props, setErrors}) {
+    setCredentials({
+      credentials: {
+        ...credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  },*/
+
+  handleSubmit(values, event, { props, setErrors }) {
+    event.preventDefault();
+    if (!values.username) {
+      setErrors({ username: "Username is required." });
+    } else if (!values.username) {
+      setErrors({ password: "Password is required" });
+    } else if (values.password && values.password.length < 6) {
+      setErrors({ password: "Password must be at least six characters" })
+      //} else if (this.validationSchema.validateAt({ email:  })) {
+      // setErrors({ email: "Please enter a valid email" });
+    } else if (!values.first_name) {
+      setErrors({ first_name: "First name is required" });
+    } else if (!values.last_name) {
+      setErrors({ last_name: "Last name is required" })
+    } else {
+      console.log("In else clause");
+
+      axiosWithAuth()
+        .post('/signup', values)
+        .then(res => {
+          console.log("Successful signup", res);
+          props.history.push('/');
+        })
+        .catch(err => console.log('Oh-oh, something wrong', err));
+    }
+  },
+
+})(SignupForm);
 export default Signup;
